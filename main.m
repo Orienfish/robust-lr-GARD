43b6882d58c70668de81bfa48fd5f6e9ff6e36c9
Orienfish <x1yu@ucsd.edu>
2020-12-13 15:57:45 -0800

    finish implementation of matrix inversion lemma

--- main.m
+++ main.m
@@ -56,6 +56,13 @@ theta_GARD_QR = GARD_QR(X, y, n, m, eps_0);
 time = toc;
 fprintf('MSE of GARD QR: %f dB, time: %f s\n', MSE(theta_0, theta_GARD_QR), time);
 
+% GARD with Matrix Inversion Lemma
+tic;
+theta_GARD_MI = GARD_MI(X, y, n, m, eps_0);
+time = toc;
+fprintf('MSE of GARD MI: %f dB, time: %f s\n', MSE(theta_0, theta_GARD_MI), time);
+
+
 function theta_GARD = GARD(X, y, n, m, eps_0)
 % GARD algorithm
 % Compute initial residual by projecting y onto R(X)
@@ -77,6 +84,39 @@ while norm(rk) > eps_0
     norm_rk_list(k) = norm(rk);
 end
 theta_GARD = z_opt(1:m);
+end
+
+function theta_GARD_MI = GARD_MI(X, y, n, m, eps_0)
+% GARD algorithm with block matrix inversion lemma
+% Compute initial residual by projecting y onto R(X)
+k = 0;
+Aac = X;
+In = eye(n);
+Aac_inv = inv(Aac'*Aac);
+phi = Aac_inv * Aac';             % Projection matrix
+z_opt = phi * y;                  % Initial opt. projection
+rk = y - Aac * z_opt;             % Initial residual
+% Start the loop until the residual is small enough
+jk_list = zeros(1, n);            % Record jk in each round
+norm_rk_list = zeros(1, n);       % Record norm(rk) in each round
+while norm(rk) > eps_0
+    k = k + 1;
+    [val, jk] = max(abs(rk));
+    jk_list(k) = jk;
+    h = phi * In(:, jk);
+    tmp = Aac * h;
+    lambda = 1 / (1 - tmp' * tmp);
+    v = In(:, jk) - tmp;
+    % Update
+    Aac = [Aac, In(:, jk)];
+    % Aac_inv = [Aac_inv + lambda * h * h', -lambda * h; ...
+    %           -lambda * h', lambda];
+    phi = [phi - lambda * h * v'; lambda * v'];
+    z_opt = phi * y;
+    rk = y - Aac * z_opt;
+    norm_rk_list(k) = norm(rk);
+end
+theta_GARD_MI = z_opt(1:m);
 end
 
 
